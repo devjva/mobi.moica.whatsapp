9dcf7eaccebc847a5c59c5f7b618a03fa9293875
Ernando <42004366+devjva@users.noreply.github.com>
2021-11-01 15:31:49 -0400

    Update Whatsapp.java

--- src/android/Whatsapp.java
+++ src/android/Whatsapp.java
@@ -9,31 +9,43 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
+import android.widget.Toast;
+import android.content.pm.PackageManager;
 
 public class Whatsapp extends CordovaPlugin {
     public static final String ACTION_WHATSAPP_SEND = "send";
     
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        try {
-            if (ACTION_WHATSAPP_SEND.equals(action)) {
-                   String send_to = args.getString(0);
-                   Uri mUri = Uri.parse("smsto:" + send_to);
-                   Intent mIntent = new Intent(Intent.ACTION_SENDTO, mUri);
-                   mIntent.setPackage("com.whatsapp");
-                   /* mIntent.putExtra("sms_body", "The text goes here"); */
-                   mIntent.putExtra("chat",true);
-
-                   this.cordova.getActivity().startActivity(mIntent);
-                   callbackContext.success();
-                   return true;
-            }
-            callbackContext.error("Invalid action");
-            return false;
-        } catch(Exception e) {
-            System.err.println("Exception: " + e.getMessage());
-            callbackContext.error(e.getMessage());
-            return false;
-        } 
-    }
+       
+            if (ACTION_WHATSAPP_SEND.equals(action)) {						
+						
+						try{
+						String numero = args.getString(0);
+						String mensaje = args.getString(1);
+						PackageManager packageManager = cordova.getActivity().getPackageManager();
+						Intent i = new Intent(Intent.ACTION_VIEW);
+						String url = "https://api.whatsapp.com/send?phone="+ numero +"&text=" + mensaje;
+						i.setPackage("com.whatsapp");
+						i.setData(Uri.parse(url));
+						if (i.resolveActivity(packageManager) != null) {
+							cordova.getActivity().startActivity(i);			
+							//this.cordova.getActivity().startActivity(mIntent);
+								   callbackContext.success();
+								   return true;
+						}else {
+							Toast.makeText(cordova.getActivity(), " ERROR" , Toast.LENGTH_SHORT);
+						}
+						
+						 
+					} catch(Exception e) {
+						Log.e("ERROR WHATSAPP",e.toString());
+						 Toast.makeText(cordova.getActivity(), " ERROR",  Toast.LENGTH_SHORT);
+						 System.err.println("Exception: " + e.getMessage());
+							callbackContext.error(e.getMessage());
+							return false;
+					}
+					
+		}return false;
+	}
 }
